534ba7cd03b563afa129abe613a3025110c3f0e3
BuildTools <unconfigured@null.spigotmc.org>
2022-11-18 15:34:44 +0100

    mejoras

--- config.js
+++ config.js
@@ -1,19 +1,9 @@
-String.prototype.hexEncode = function(){
-    var hex, i;
-    var result = "";
-    for (i=0; i<this.length; i++) {
-        hex = this.charCodeAt(i).toString(16);
-        result += ("000"+hex).slice(-4);
-    }
-    return result
-}
 String.prototype.hexDecode = function(){
-    var j;
-    var hexes = this.match(/.{1,4}/g) || [];
-    var back = "";
-    for(j = 0; j<hexes.length; j++) {
-        back += String.fromCharCode(parseInt(hexes[j], 16));
-    }
+    let hexes = this.match(/.{1,4}/g) || [];
+    let back = new String();
+    hexes.forEach(function(value) {
+        back += String.fromCharCode(parseInt(value, 16));
+    })
     return back;
 }
 getToken = function(token) {
